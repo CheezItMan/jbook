e364829c128bc0d630c3d197352eeb5ddbc746a2
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-22 19:27:04 -0700

    initial index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from 'esbuild-wasm';
+import ReactDom from 'react-dom';
+import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const App = () => {
+
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const serviceRef = useRef<any>();
+
+    const startService = async () => {
+        serviceRef.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm',
+        });
+    }
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setInput(event.target.value)
+    }
+
+    const onClick = async () => {
+        console.log(input);
+        if (!serviceRef.current) return;
+
+        console.log(serviceRef.current);
+
+        const result = await serviceRef.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()],
+        })
+        console.log(result);
+        setCode(result.outputFiles[0].text);
+    }
+
+    return <div>
+        <textarea onChange={onInputChange} value={input}></textarea>
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));

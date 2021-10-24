02e495c1b766c4d4610ee581627928560e8d4811
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-24 16:00:25 -0700

    update to handle imports and require stmts

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -11,14 +11,19 @@ export const unpkgPathPlugin = () => {
         // Detect if we need to get the path for an npm package
         if (args.path === 'index.js') {
             return { path: args.path, namespace: 'a'}
-        }        
-        if (args.path === 'tiny-test-pkg') {
-            return { path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js', namespace: 'a'}
         }
-        // In that case use unPKG
 
-
-        return { path: args.path, namespace: 'a' };
+        if (args.path.includes('./') || args.path.includes('../')) {
+            return {
+                namespace: 'a',
+                path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+            };
+        } 
+        
+        return {
+            namespace: 'a',
+            path: `https://unpkg.com/${args.path}`,
+        }        
       });
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -28,17 +33,18 @@ export const unpkgPathPlugin = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from 'tiny-test-pkg';
+              import React from 'react';
               console.log(message);
             `,
           };        
         }
-        const { data } = await axios.get(args.path);
+        const { data, request } = await axios.get(args.path);
         console.log(data);
 
         return {
             loader: 'jsx',
             contents:  `${data}`,
+            resolveDir: new URL('./', request.responseURL).pathname,
         }
       });
     },
